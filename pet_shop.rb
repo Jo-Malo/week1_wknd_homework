be180e529d8bd0017c3183d3d5ec609c7821cb53
jo-emalo <37541560+jo-emalo@users.noreply.github.com>
2018-04-30 20:31:11 +0100

    Update pet_shop.rb

--- pet_shop.rb
+++ pet_shop.rb
@@ -49,6 +49,22 @@ def add_pet_to_stock(shop, pet)
   shop[:pets].push(pet)
 end
 
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets] << pet
+end
+
 
 
 end
